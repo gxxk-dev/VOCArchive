204d0f6b1bb5c340dfd3dc5bb79ff0cfd1b38849
Gxxk_ <gxxk@duck.com>
2025-07-17 16:36:05 +0800

    add: 构建系统

--- src/build.py
+++ src/build.py
@@ -1,4 +1,45 @@
-import os
+import os,yaml,re,shutil,json
+from utils import FindFile,Path2Replace
 
-def Build(songPackPath:str):
-    songList=os.listdir(songPackPath)
+filePathList=[]
+
+def Build_CollectFilePath(_1,_2,path):
+    """收集文件路径."""
+    global filePathList
+    filePathList.append(path)
+
+def Build(songPackPath:str,outputPath:str="./build",link2copy=False):
+    """主入口.
+
+    args:
+        songPackPath: 歌曲包路径
+        outputPath: 输出路径
+        link2copy: 是否使用链接代替复制
+    """
+    os.makedirs('build/res', exist_ok=True)
+    pattern=re.compile(r'.*info[^\\/]*\.(ya?ml)$', re.IGNORECASE)
+    # 歌曲信息处理
+    for song in FindFile(songPackPath,pattern):
+        songPath=os.path.join(songPackPath,song)
+
+        if not os.path.isdir(songPath): #类型检查
+            raise UserWarning() # 非目录报错 
+        
+        with open(os.path.join(songPath,"info.yaml"),'r',encoding='utf-8') as f:
+            info=yaml.safe_load(f)
+        # Hash...
+
+        info["files"]=Path2Replace(info,songPath,BeforeReplace=songPackPath)
+    # 写入歌曲信息
+    with open(os.path.join(outputPath,"song.json"), "w", encoding="utf-8") as f:
+        json.dump(info, f)
+
+    # 歌曲文件操作
+    for path in filePathList:
+        if link2copy:
+            os.symlink(path,os.path.join(outputPath,path))
+        else:
+            shutil.copy(path,os.path.join(outputPath,path))
+    
+    with open(os.path.join(outputPath,"info.json"), "w", encoding="utf-8") as f:
+        f.write('{"apiEnable":false,"apiUrl":"","songInfoUrl":"/song.json","resUrl":"/res"}')
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,87 @@
-import hashlib
+import hashlib,os,re,shutil
+from typing import Callable
 
-def file_hash(file_path: str, hash_method=hashlib.sha512) -> str:
-    h = hash_method()
-    with open(file_path, 'rb') as f:
+def FileHash(filePath: str, HashMethod=hashlib.sha512) -> str:
+    """Hash文件
+    args:
+        file_path: 文件路径
+        hash_method: [可被调用]hash方法
+    return:
+        str: hash值"""
+    h = HashMethod()
+    with open(filePath, 'rb') as f:
         while b := f.read(8192):
             h.update(b)
     return h.hexdigest()
+
+def FindFile(rootDir:str,pattern:re.Pattern)->list:
+    """递归查找文件名符合设定的文件
+
+    args:
+        rootDir: 查找的根目录
+        pattern: 用于匹配文件名的正则
+    return:
+        list: 匹配到的文件列表
+    """
+    infoFiles=[]
+    for root, _, files in os.walk(rootDir):
+        for file in files:
+            if pattern.match(file):
+                infoFiles.append(os.path.join(root, file))
+    return infoFiles
+
+def Path2Replace(obj, rootDir:str,ReplaceMethod:Callable[[str],str]=FileHash,BeforeReplace:Callable[[str,str,str],None]=None):
+    """递归遍历对象 替换文件路径为指定对象
+    
+    args:
+        obj: 待处理对象
+        rootDir: 根目录
+        ReplaceMethod: 决定替换内容的函数 传入/传出为替换前/替换后内容
+        BeforeReplace: 替换前处理方法 传入为替换前内容/替换后内容/路径
+    return:
+        obj: 处理后的对象
+    """ 
+    # TODO: 基于Py可变对象的特性优化此处代码
+    if isinstance(obj, dict):
+        # dict:递归处理
+        return {k: Path2Replace(v, rootDir) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        # 同上 懒得写注释了()
+        return [Path2Replace(item, rootDir) for item in obj]
+    elif isinstance(obj, str):
+        if os.path.isabs(obj): # 完整路径 直接算
+            OriginContent,ReplaceContent=obj,ReplaceMethod(obj)
+            if BeforeReplace is not None:
+                BeforeReplace(OriginContent,ReplaceContent,os.path.join(rootDir, obj))
+            return ReplaceMethod
+        else:
+            return Path2Replace(os.path.join(rootDir, obj))
+    else:
+        raise NotImplementedError()
+        #非预期情况 懒得适配()
+
+def CleanDir(dirPath):
+    """清空目标目录（文件、硬链接、软链接）"""
+    if not os.path.isdir(dirPath):
+        return NotImplementedError()
+    
+    # 递归清空内容
+    for item in os.listdir(dirPath):
+        item_path = os.path.join(dirPath, item)
+        
+        # 符号链接：直接删除链接
+        if os.path.islink(item_path):
+            os.unlink(item_path)
+            
+        # 目录：递归清空并删除
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path, ignore_errors=True)
+            
+        # 文件/硬链接：直接删除
+        else:
+            try:
+                os.remove(item_path)
+            except:
+                # 只读文件处理
+                os.chmod(item_path, 0o777)  # 给予完全权限
+                os.remove(item_path)
